866d278f75475b3c13b3483edbe2139ecea127a8
tinqnit <tinqnit@gmail.com>
2020-03-24 14:04:36 +0530

    NAME->[OUTPOINT]

--- client.py
+++ client.py
@@ -67,38 +67,42 @@ sock = client(sys.argv[1], sys.argv[2])
 
 while True:
 
-    x1 = dumps((0, 1, 'HEIGHT->BLOCK', [(0, 100)]))
+    x0 = dumps((0, 1, 'HEIGHT->BLOCK', [(0, 100)]))
 
-    x2 = dumps((0, 1, '[HEIGHT]->[BLOCK]', [(1, [100, 101, 102])]))
+    x1 = dumps((0, 1, '[HEIGHT]->[BLOCK]', [(1, [100, 101, 102])]))
 
-    x3 = dumps((0, 1, 'HASH->BLOCK', [(2,
+    x2 = dumps((0, 1, 'HASH->BLOCK', [(2,
                                        '00000000d1145790a8694403d4063f323d499e655c83426834d4ce2f8dd4a2ee'
                                        )]))
 
-    x4 = dumps((0, 1, '[HASH]->[BLOCK]', [(3,
+    x3 = dumps((0, 1, '[HASH]->[BLOCK]', [(3,
                                            ['000000000000000002af2a6de04d4a1a73973827eae348fe4d3f4d05610ff968',
                                             '000000000000000007fc734cbf1fc04c59cf7ecb6af0707fd5cf5b8d46dc4c75'
                                             ])]))
 
-    x5 = dumps((0, 1, 'TXID->TX', [(4,
+    x4 = dumps((0, 1, 'TXID->TX', [(4,
                                     '7d3eb236b526bd681b7fc499d657d237b4d3bc21ef25b37fc1c70822849f1243'
                                     )]))
 
-    x6 = dumps((0, 1, '[TXID]->[TX]', [(5,
+    x5 = dumps((0, 1, '[TXID]->[TX]', [(5,
                                         ['6c828920ea3a968f0c3c4a8f14d70b696e0440d8e4e1d019cced1ba2cc63cd51',
                                          '097cf9d4ec10711e809f316b7738bbbff94efe32ea2cd55e57ddf5840f828741'
                                          ])]))
 
-    x7 = dumps((0, 1, 'ADDR->[OUTPUT]', [(6,
+    x6 = dumps((0, 1, 'ADDR->[OUTPUT]', [(6,
                                           '13n561iVozTtMXJzAJNA5TQsnTboRvpxae')]))
 
-    x8 = dumps((0, 1, '[ADDR]->[OUTPUT]', [(7,
+    x7 = dumps((0, 1, '[ADDR]->[OUTPUT]', [(7,
                                             ['1P8Jd8qQM7y45iXLM1eiXCCmGRhCPjykZB',
                                              '16qgC3hzi38xo1vn2gGsNVwWaW1sEH3h9R'])]))
 
-    x9 = dumps((0, 1, 'TXID->[MNODE]', [(8,
+    x8 = dumps((0, 1, 'TXID->[MNODE]', [(8,
                                          '571c7508413415debe4ba146a2ed141e4d4204d0743169ab3366b1f1e1960a5d'
                                          )]))
+
+    x9 = dumps((0, 1, 'NAME->[OUTPOINT]', [(9,
+                                            '[h', True
+                                            )]))
 
     c = Bitcoin()
     priv = sha256('allegory allpay test dummy seed')
@@ -143,7 +147,7 @@ while True:
     print(txhash(txser))
     # save it for later use
     firstTxHash = txhash(txser)
-    x10 = dumps((0, 1, 'RELAY_TX', [(9, bytes.fromhex(txser))]))
+    x10 = dumps((0, 1, 'RELAY_TX', [(10, bytes.fromhex(txser))]))
 
 ###########
 
@@ -176,7 +180,7 @@ while True:
     print(txser)
     print(txhash(txser))
     secondTxHash = txhash(txser)
-    x11 = dumps((0, 1, 'RELAY_TX', [(9, bytes.fromhex(txser))]))
+    x11 = dumps((0, 1, 'RELAY_TX', [(10, bytes.fromhex(txser))]))
 
 ############
 
@@ -216,8 +220,9 @@ while True:
     print(txhash(txser))
     # save it for later use
     firstTxHash = txhash(txser)
-    x12 = dumps((0, 1, 'RELAY_TX', [(9, bytes.fromhex(txser))]))
+    x12 = dumps((0, 1, 'RELAY_TX', [(10, bytes.fromhex(txser))]))
     #
+
     #
 
     # x10 = dumps((0, 1, 'RELAY_TX', [(9, bytes.fromhex(''))]))
@@ -243,7 +248,7 @@ while True:
     # sendRequest(sock, x11)
     # recvResponse(sock)
     print("***************************")
-    sendRequest(sock, x12)
+    sendRequest(sock, x9)
     recvResponse(sock)
     # recvResponse(sock)
     # recvResponse(sock)
