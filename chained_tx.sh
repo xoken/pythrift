d4d4ed2bdc1a84d673b517e9beab9d90d0bbd7c1
Nishant Kaushik <nixantkaushik@gmail.com>
2020-11-07 12:22:19 +0530

    Update chained_tx.sh

--- chained_tx.sh
+++ chained_tx.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-for i in {1..5}
+for i in 1
 do
 init_addr=$(bitcoin-cli -regtest getnewaddress)
 vout=0
-val=25
+val=3
 self=0.999995
 init_tx=$(bitcoin-cli -regtest sendtoaddress $init_addr $val)
 txid=$init_tx
-#nb=$( bitcoin-cli -regtest generatetoaddress 1 $(bitcoin-cli -regtest getnewaddress))
-for j in {1..24}
+echo $txid
+nb=$( bitcoin-cli -regtest generatetoaddress 1 $(bitcoin-cli -regtest getnewaddress))
+echo $nb
+sleep 5
+for j in {1..2}
 do
 amt=$(python -c "print($val - 1)")
 chg_addr=$(bitcoin-cli -regtest getnewaddress)
@@ -34,5 +37,6 @@ s_tx=$(bitcoin-cli -regtest signrawtransaction $raw_tx | jq -r .hex)
 txid=$(bitcoin-cli -regtest sendrawtransaction $s_tx)
 echo $txid
 val=$amt
+sleep 5
 done
 done
